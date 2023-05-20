bbb232d3b551463780b69541826343ded76264ed
Rujikorn Khampho <jean@macbook-air.local>
2023-05-21 00:35:03 +0700

    feat: initial ProfileSerializer class

--- backend/core/authentication/serializers.py
+++ backend/core/authentication/serializers.py
@@ -1,6 +1,47 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from core.authentication.utils import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'first_name',
+            'last_name',
+            'phone',
+            'address',
+            'password',
+            'confirm_password',
+        ]
+
+    def validate(self, data):
+        if data.get('password') != data.get('confirm_password'):
+            raise serializers.ValidationError('Password did not match')
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data.get('email'),
+            first_name=validated_data.get('first_name'),
+            last_name=validated_data.get('last_name'),
+            phone=validated_data.get('phone'),
+            address=validated_data.get('address'),
+        )
+        user.password = make_password(validated_data.get('password'))
+        user.save()
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['password']
 
 
 class UserSerializer(serializers.ModelSerializer):
