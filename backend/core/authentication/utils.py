0e66d946e7ad58e472ec30a57395d2a71f39643c
Rujikorn Khampho <jean@macbook-air.local>
2023-05-27 23:29:16 +0700

    refactor: remove the ListOnlyModelViewSet class

--- backend/core/authentication/utils.py
+++ backend/core/authentication/utils.py
@@ -1,18 +1,3 @@
 from django.contrib.auth import get_user_model
-from rest_framework import mixins, viewsets
 
 User = get_user_model()
-
-
-class CreateOnlyModelViewSet(
-    mixins.CreateModelMixin,
-    viewsets.GenericViewSet,
-):
-    pass
-
-
-class ListOnlyModelViewSet(
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
-    pass
