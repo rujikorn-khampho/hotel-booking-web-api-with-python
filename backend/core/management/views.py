9062094885bcbd5999709aef887ca65ee79e1793
Rujikorn Khampho <jean@macbook-air.local>
2023-06-17 20:40:14 +0700

    perf: query optimization

--- backend/core/management/views.py
+++ backend/core/management/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 
-from core.management.models import Hotel, RoomType, Equipment
+from core.management.models import Hotel, RoomType, Equipment, Booking
 from core.management.serializers import (
+    BookingReadSerializer,
+    BookingWriteSerializer,
     EquipmentSerializer,
     HotelListSerializer,
     HotelRetrieveSerializer,
@@ -26,9 +28,9 @@ class HotelViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         serializer_action_mapping = {
             'create': self.write_serializer_class,
+            'retrieve': self.retrieve_serializer_class,
             'update': self.write_serializer_class,
             'partial_update': self.write_serializer_class,
-            'retrieve': self.retrieve_serializer_class,
         }
 
         return serializer_action_mapping.get(
@@ -47,3 +49,35 @@ class EquipmentViewSet(viewsets.ModelViewSet):
     queryset = Equipment.objects.order_by('pk')
     serializer_class = EquipmentSerializer
     permission_classes = [IsAdminUser]
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.prefetch_related(
+        'room__amenities__equipment',
+    ).select_related(
+        'room__type',
+        'guest',
+    ).order_by(
+        'pk',
+    )
+    write_serializer_class = BookingWriteSerializer
+    read_serializer_class = BookingReadSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser is False:
+            return super().get_queryset().filter(
+                guest=self.request.user,
+            )
+        return super().get_queryset()
+
+    def get_serializer_class(self):
+        serializer_action_mapping = {
+            'create': self.write_serializer_class,
+            'update': self.write_serializer_class,
+            'partial_update': self.write_serializer_class,
+        }
+
+        return serializer_action_mapping.get(
+            self.action,
+            self.read_serializer_class,
+        )
