abffa6c4ec41ceaf1fb04c2f5ee5ebccc010db8a
Rujikorn Khampho <jean@macbook-air.local>
2023-06-17 20:39:26 +0700

    feat: create the booking serializer

--- backend/core/management/serializers.py
+++ backend/core/management/serializers.py
@@ -1,7 +1,8 @@
 from drf_writable_nested.serializers import WritableNestedModelSerializer
 from rest_framework import serializers
 
-from core.management.models import Hotel, RoomType, Equipment, Room, Amenity
+from core.authentication.serializers import GuestReadSerializer
+from core.management.models import Hotel, RoomType, Equipment, Room, Amenity, Booking
 
 
 class AmenityWriteSerializer(serializers.ModelSerializer):
@@ -18,6 +19,7 @@ class RoomWriteSerializer(WritableNestedModelSerializer):
         exclude = [
             'hotel',
             'equipments',
+            'guests',
         ]
 
 
@@ -56,3 +58,35 @@ class HotelRetrieveSerializer(HotelWriteSerializer):
 
 class HotelListSerializer(HotelRetrieveSerializer):
     rooms = None
+
+
+class BookingWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        exclude = ['guest']
+
+    @staticmethod
+    def validate_room(value):
+        if not value.is_available:
+            raise serializers.ValidationError('This room is not available')
+        return value
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        validated_data['guest'] = request.user
+        self.change_room_status(validated_data.get('room'))
+        return super().create(validated_data)
+
+    @staticmethod
+    def change_room_status(room):
+        room.is_available = False
+        room.save()
+
+
+class BookingReadSerializer(serializers.ModelSerializer):
+    room = RoomReadSerializer()
+    guest = GuestReadSerializer()
+
+    class Meta:
+        model = Booking
+        fields = '__all__'
